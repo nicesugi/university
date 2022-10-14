d2c3ba521117729fcf4754129f73b74ab00e5d57
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-10-14 17:57:53 +0900

    feat/ 로그인 기능 구현

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 from users import views
 
 urlpatterns = [
     path('', views.UserView.as_view()),
+    path('login', TokenObtainPairView.as_view()),
 ]
