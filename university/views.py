9a431fac5b8ebc8da35aa7e26df7340de39cbcc4
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-11-24 23:00:53 +0900

    refac/ status code 수정 및 requirements.txt 업데이트

--- university/views.py
+++ university/views.py
@@ -11,10 +11,10 @@ from university.serializers import UniversitySerializer
 
 class TaskView(APIView):
     """
-    웹 페이지에서 제공하는 Json Data를 토대로 데이터 삽입
+    post : 웹 페이지에서 제공하는 Json Data를 토대로 데이터 삽입
     """
 
-    def get(self, request):
+    def post(self, request):
         url = requests.get("http://universities.hipolabs.com/search")
         text = url.text
         data = json.loads(text)
@@ -47,19 +47,19 @@ class TaskView(APIView):
                     user=user, university_id=randrange(1, 1000)
                 )
 
-        return Response({"detail": "정보 저장이 완료되었습니다"}, status=status.HTTP_200_OK)
+        return Response({"detail": "정보 저장이 완료되었습니다"}, status=status.HTTP_201_CREATED)
 
 
 class SearchView(APIView):
     def get(self, request):
         """
         대학교 검색
-            search (str) : 대학교 이름이나 국가코드 검색
-            page_limit (int) : 한 페이지에 보여지는 게시글 수
-            page (int) : 보고자하는 페이지
+        search (str) : 대학교 이름이나 국가코드 검색
+        page_limit (int) : 한 페이지에 보여지는 게시글 수
+        page (int) : 보고자하는 페이지
 
-            return = 검색어 있을 때 : 적용된 대학교의 pk값 내림차순을 최대 10개까지 페이징처리된 serializer.data
-                    검색어 없을 때 : '검색어가 비어있습니다' 알림
+        return = 검색어 있을 때 : 적용된 대학교의 pk값 내림차순을 최대 10개까지 페이징처리된 serializer.data
+                검색어 없을 때 : '검색어가 비어있습니다' 알림
         """
         search = self.request.GET.get("search", "")
         if search == "":
@@ -80,10 +80,12 @@ class SearchView(APIView):
 
 
 class RreferenceView(APIView):
+    """
+    post : 선호 대학 등록
+    delete : 선호 대학 삭제(soft delete)
+    """
+
     def post(self, request, university_id):
-        """
-        선호 대학 등록
-        """
         user = request.user
         university = University.objects.get(id=university_id)
 
@@ -93,21 +95,25 @@ class RreferenceView(APIView):
 
         if created_obj:
             if getted_obj.user.universitypreference_set.count() <= 20:
-                return Response({"detail": "선호 대학으로 등록했습니다"}, status=status.HTTP_200_OK)
+                return Response(
+                    {"detail": "선호 대학으로 등록했습니다"}, status=status.HTTP_201_CREATED
+                )
             else:
                 return Response(
-                    {"detail": "선호 대학은 20개까지만 가능합니다"}, status=status.HTTP_200_OK
+                    {"detail": "선호 대학은 20개까지만 가능합니다"},
+                    status=status.HTTP_400_BAD_REQUEST,
                 )
         elif getted_obj.is_active == False:
             getted_obj.is_active == True
             getted_obj.save()
-            return Response({"detail": "선호 대학으로 등록했습니다"}, status=status.HTTP_200_OK)
-        return Response({"detail": "이미 선호 대학목록에 있습니다"}, status=status.HTTP_200_OK)
+            return Response(
+                {"detail": "선호 대학으로 등록했습니다"}, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            {"detail": "이미 선호 대학목록에 있습니다"}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     def delete(self, request, university_id):
-        """
-        선호 대학 삭제(soft delete)
-        """
         user = request.user
         university = University.objects.get(id=university_id)
 
